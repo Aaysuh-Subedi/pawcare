370c1c698f30b76d509ff0177502c966ffdb1b5e
Aasu703 <smellylips508@gmail.com>
2025-12-27 11:35:00 +0545

    schemas

---
+++ app/(auth)/_components/LoginForm.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import Link from "next/link";
---
+++ app/(auth)/schema.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+    email: z.email({ message:"Please enter a valid email"}),
+    password: z.string().min(8, {message : "Password must be of 8 characters"}),
+});
+
+export type LoginData = z.infer<typeof loginSchema>;
+
+export const registerSchema = z.object({
+    Firstname : z.string().min(2, {message : "Invalid name"  }),
+    Lastname :z.string().min(2, {message :"Invalid name"}),
+    email: z.email({ message:"Please enter a valid email"}),
+    password: z.string().min(8, {message : "Password must be of 8 characters"}),
+    confirmPassword: z.string().min(8, {message : "Password must be of 8 characters"}),
+}).refine((v) => v.password === v.confirmPassword,{
+    path: ["confirmPassword"],
+    message : "Please match the passwords..."
+});
+
+export type RegisterData = z.infer<typeof registerSchema>;
+
